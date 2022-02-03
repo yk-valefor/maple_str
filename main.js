b258229a57bc62a21926cb7c5f3e387391594180
Maple.F <87688106+yk-valefor@users.noreply.github.com>
2022-02-03 22:35:25 +0900

    skill

--- main.js
+++ main.js
@@ -35,13 +35,31 @@ const myAllSkills = [
     id: 1,
     icon: "./icon/html-5.svg",
     skillname: "HTML5",
-    text: "小学生の時に個人サイトを作ったりしていました。簡単なものはフルスクラッチで書けます。テンプレートも必要な箇所を確認し、カスタマイズできます。",
+    text: "小学生の時に個人サイトを作ったりしていました。簡単なものはフルスクラッチで書けます。テンプレートでは必要な箇所を確認し、カスタマイズできます。",
   },
   {
     id: 2,
     icon: "./icon/css-3.svg",
     skillname: "CSS3",
-    text: "小学生の時に個人サイトを作ったりしていました。簡単なものはフルスクラッチで書けます。テンプレートも必要な箇所を確認し、カスタマイズできます。",
+    text: "簡単なものはフルスクラッチで書けます。Bootstrapなど必要なライブラリをカスタマイズして使用できます。Sassを使用しています。",
+  },
+  {
+    id: 3,
+    icon: "./icon/javascript.svg",
+    skillname: "JavaScript",
+    text: "簡単な機能なら実装できます。JQueryでアニメーションなどを書けます。APIが実装できます。",
+  },
+  {
+    id: 4,
+    icon: "./icon/vue.svg",
+    skillname: "Vue.js",
+    text: "データバインディング、イベントハンドリング、コンポーネントを実装できます。",
+  },
+  {
+    id: 5,
+    icon: "./icon/react.svg",
+    skillname: "React.js",
+    text: "勉強中です。簡単なメモアプリなら作成できます。",
   },
 ];
 
